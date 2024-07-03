e0726b106b0855d8b7d6b1f1d88cde0f8b5bd94e
Alex1911 <tjxx_jgd@163.com>
2024-07-03 18:14:16 +0800

    Update OkxConstants.cs

--- OKX.Api/OkxConstants.cs
+++ OKX.Api/OkxConstants.cs
@@ -8,5 +8,5 @@ internal static class OkxConstants
     // Local Settings
     public static CultureInfo OkxCultureInfo = CultureInfo.InvariantCulture;
     public const string OkxDatetimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
-    public const string BrokerId = "538a3965e538BCDE";
+    public const string BrokerId = "cb6f16849514BCDE";
 }
